db18b169ee5b70c37ded088e3e767b963ae53a64
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-15 02:18:23 +0100

    about

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./About.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+
+const abouts = [
+  {
+    title: "Frontend Development",
+    description: "I create responsive web pages",
+    imgUrl: images.about01,
+  },
+  {
+    title: "Web Animations",
+    description: "I make animations that enhance user interfaces",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Software Testing",
+    description:
+      "I test web applications to make sure the outcome is as anticipated.",
+    imgUrl: images.about04,
+  },
+];
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text">
+        I understand that a <span>Successful Business</span> <br /> is a Result
+        <span> of effective development</span>
+      </h2>
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
