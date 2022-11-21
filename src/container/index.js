d7ce300d01541de302a60689047aa7191d0d8a34
jam-jam200 <jessieoyaks4j@gmail.com>
2022-11-21 23:50:56 +0100

    changes

--- src/container/index.js
+++ src/container/index.js
@@ -1,6 +1,6 @@
-export { default as About } from "./About/About";
-export { default as Footer } from "./Footer/Footer";
-export { default as Header } from "./Header/Header";
-export { default as Skills } from "./Skills/Skills";
-export { default as Testimonial } from "./Testimonial/Testimonial";
+export { default as About } from "../../frontend/src/container/About/About";
+export { default as Footer } from "../../frontend/src/container/Footer/Footer";
+export { default as Header } from "../../frontend/src/container/Header/Header";
+export { default as Skills } from "../../frontend/src/Skills/Skills";
+export { default as Testimonial } from "../../frontend/src/container/Testimonial/Testimonial";
 export { default as Work } from "./Work/Work";
